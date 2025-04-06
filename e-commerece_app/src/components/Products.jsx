ed5c598893722a73b8bad432f49c9db5d2096bd2
vikraman-mani <vikraman02032001@gmail.com>
2025-04-06 18:12:57 +0530

    BootStrap

--- e-commerece_app/src/components/Products.jsx
+++ e-commerece_app/src/components/Products.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
+
 
 const Products = () => {
   return (
@@ -8,6 +10,7 @@ const Products = () => {
         <Link to="list"> Product List </Link>
         <Link to="details"> Product Details </Link>
         <Outlet/>
+        <Button variant="primary"> Increase </Button>
     </div>
   )
 }
