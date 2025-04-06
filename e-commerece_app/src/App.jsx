5e016a709555f63fc7fad10760b3f883c4dd2759
vikraman-mani <vikraman02032001@gmail.com>
2025-04-06 17:22:18 +0530

    React-Router & Route-Link

--- e-commerece_app/src/App.jsx
+++ e-commerece_app/src/App.jsx
@@ -3,16 +3,28 @@ import Home from './components/Home'
 import Login from './components/Login'
 import Products from './components/Products'
 import SignUp from './components/SignUp'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 
 function App() {
-
-  
   return (
-     <div className='App'>
-        <Home/>
-        <Login/>
-        <Products/>
-        <SignUp/>
+     <div className='app'>
+
+        <BrowserRouter>
+            <ol>
+              <li>  <Link to="/">Home</Link>  </li>
+              <li>  <Link to="/login">Login</Link>  </li>
+              <li>  <Link to="/signup">SignUp</Link>  </li>
+              <li>  <Link to="/products">Products</Link>  </li>
+            </ol>
+
+            <Routes>
+                 <Route path="/" element={<Home/>} />
+                 <Route path="/login" element={<Login/>} />
+                 <Route path="/signup" element={<SignUp/>} />
+                 <Route path="/products" element={<Products/>} />
+            </Routes>
+
+        </BrowserRouter>
      </div>
   )
 }
