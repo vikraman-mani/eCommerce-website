5c15a057b7dcfe64639f5d714efdd2ac64e6a41f
vikraman-mani <vikraman02032001@gmail.com>
2025-04-10 13:07:27 +0530

    Add cart icons

--- e-commerece_app/src/components/NavBar.jsx
+++ e-commerece_app/src/components/NavBar.jsx
@@ -1,28 +1,40 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom';
+import { Button } from "@mui/material";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import { MdShoppingCart } from "react-icons/md";
 
 function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
-        <Navbar.Toggle aria-controls="navbarScroll" />  {/* <!-- Toogle used for mobile view --> */}
+        <Navbar.Toggle aria-controls="navbarScroll" />{" "}
+        {/* <!-- Toogle used for mobile view --> */}
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/login/Vikraman">Login</Nav.Link>
-            <Nav.Link as={Link} to="/signup">Sign-UP</Nav.Link>
-            <Nav.Link as={Link} to="/products">Products</Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/login/Vikraman">
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to="/signup">
+              Sign-UP
+            </Nav.Link>
+            <Nav.Link as={Link} to="/products">
+              Products
+            </Nav.Link>
           </Nav>
           <Form className="d-flex">
+            <Button variant="contained" color="warning">
+              <MdShoppingCart />
+            </Button>
             <Form.Control
               type="search"
               placeholder="Search"
